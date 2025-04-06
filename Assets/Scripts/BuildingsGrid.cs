6f8f4e0b8e174e043301d2565f5dce7dd1a9a30d
Dubrovin Vyacheslav <via4eslav.dubrovin@yandex.ru>
2025-04-06 19:36:09 +0500

    Закончена система строительства

--- Assets/Scripts/BuildingsGrid.cs
+++ Assets/Scripts/BuildingsGrid.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.LightTransport;
 using UnityEngine.Tilemaps;
 
 public class BuildingsGrid : MonoBehaviour
@@ -12,7 +12,7 @@ public class BuildingsGrid : MonoBehaviour
     private Camera cam;
     private Grid grid;
     private Building flyingBuilding;
-    private bool[,] map; // надо доделать, это дл€ корректной проверки зан€тости, когда ставим предмет, то тут занимаем облость, через зан€тие тайлов не удобно провер€ть
+    private Dictionary<Vector3Int, Vector2Int> busyPositions = new(); // positition left bottom : size
 
     private void Awake()
     {
@@ -39,11 +39,12 @@ public class BuildingsGrid : MonoBehaviour
             flyingBuilding.transform.position = new Vector3Int(pos.x + 1, pos.y + 1, 0);
             if (Input.GetKeyDown(KeyCode.Mouse0))
             { 
-                bool canPlace = CheckTileToPlaceBuilding(pos);
+                bool canPlace = CheckToPlaceBuilding(pos);
                 Debug.Log(canPlace);
                 if (canPlace)
                 {
                     buildingsTilemap.SetTile(grid.WorldToCell(MousePos), flyingBuilding.GetTile());
+                    busyPositions.Add(pos, flyingBuilding.Size);
                     StopPlacingBuilding();
                 }
             }
@@ -55,18 +56,18 @@ public class BuildingsGrid : MonoBehaviour
         Destroy(flyingBuilding.gameObject);
     }
 
-    private bool CheckTileToPlaceBuilding(Vector3Int gridPlace)
+    private bool CheckToPlaceBuilding(Vector3Int gridPlace)
     {
-        for (int i = 0; i < flyingBuilding.Size.x; i++)
+        Rect rect1 = new(gridPlace.x, gridPlace.y, flyingBuilding.Size.x, flyingBuilding.Size.y);
+        foreach (Vector3Int pos in busyPositions.Keys)
         {
-            for (int j = 0; j < flyingBuilding.Size.y; j++)
+            Rect rect2 = new(pos.x, pos.y, busyPositions[pos].x, busyPositions[pos].y);
+            if (rect1.Overlaps(rect2))
             {
-                if (buildingsTilemap.HasTile(gridPlace + new Vector3Int(i, j, 0)))
-                {
-                    return false;
-                }
+                return false;
             }
         }
+
         return true;
     }
 }
